e8f5ee4d59f10ac74f3c97dbee9b27111f87a75d
KevinKener <kevinkener.kick@outlook.com>
2023-09-08 19:03:02 -0300

    version final

--- cod_generator.py
+++ cod_generator.py
@@ -1,8 +1,9 @@
 import random
 import string
 
+
 # get random password pf length 8 with letters, digits, and symbols
 def generar():
     characters = string.ascii_letters + string.digits
-    cod = ''.join(random.choice(characters) for i in range(8))
+    cod = "".join(random.choice(characters) for i in range(8))
     return cod
--- libro.py
+++ libro.py
@@ -1,31 +1,4 @@
-from cod_generator import generar
-
 # Crear un diccionario para cada libro
-libro1 = {'cod': 'CRBJsAkS', 'cant_ej_ad': 3, 'cant_ej_pr': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
-libro2 = {'cod': 'QgfV4j3c', 'cant_ej_ad': 4, 'cant_ej_pr': 2, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
-libro3 = {'cod': 'adOd09UE', 'cant_ej_ad': 1, 'cant_ej_pr': 0, "titulo": "El código Da Vinci", "autor": "Dan Brown"}
-
-def generar_codigo():
-    codigo_generado = generar()
-    return codigo_generado
-
-def nuevo_libro(autor, titulo, cant_ej_adquiridos):
-    #Creacion del diccionario para el nuevo libro y sus respectivos datos.
-    codigo = generar_codigo()  
-    libro_nuevo = {
-        'cod': codigo,
-        'autor': autor,
-        'titulo': titulo,
-        'cant_ej_ad': cant_ej_adquiridos,
-        'cant_ej_pr': 0
-        }  
-
-    
-    print(f"""
-          Titulo: {titulo}
-          Autor: {autor}
-          Código: {codigo}
-          Cantidad de ejemplares adquiridos: {cant_ej_adquiridos}
-          """)    
-
-    return libro_nuevo
+libro1 = {'cod': 'CRBJsAkS', 'cantEjemplares': 3, 'cantEjPrestados': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
+libro2 = {'cod': 'QgfV4j3c', 'cantEjemplares': 4, 'cantEjPrestados': 2, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
+libro3 = {'cod': 'adOd09UE', 'cantEjemplares': 1, 'cantEjPrestados': 0, "titulo": "El código Da Vinci", "autor": "Dan Brown"}
